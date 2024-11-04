7d159639306f3934673bdc2f443c5abbe9f9f11b
mostofa-rezvi <mostofa.aminur.rezvi@gmail.com>
2024-11-04 19:03:57 +0600

    part 6 (half)

--- demo_flutter/lib/service/AuthService.dart
+++ demo_flutter/lib/service/AuthService.dart
@@ -1,3 +1,79 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:jwt_decode/jwt_decode.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 class AuthService {
-  final String 
+  final String baseUrl = 'http://localhost:8080';
+
+  Future<bool> login(String email, String password) async {
+
+    final url = Uri.parse('$baseUrl/login');
+    final headers = {'Content-Type': 'application/json'};
+    final body = jsonEncode({'email': email, 'password': password});
+
+    final response = await http.post(url, headers: headers, body: body);
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      String token = data['token'];
+
+      Map<String, dynamic> payload = Jwt.parseJwt(token);
+      String role = payload['role'];
+
+      SharedPreferences preferences = await SharedPreferences.getInstance();
+      await preferences.setString('authToken', token);
+      await preferences.setString('userRole', role);
+
+      return true;
+    }
+    else {
+      print('Failed to log in: ${response.body}');
+      return false;
+    }
+  }
+
+  Future<bool> register(Map<String, dynamic> user) async {
+    final url = Uri.parse('$baseUrl/register');
+    final header = {'Content-Type': 'application/json'};
+    final body = jsonEncode(user);
+
+    final response = await http.post(url, headers: header, body: body);
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      String token = data['token'];
+
+      SharedPreferences preferences = await SharedPreferences.getInstance();
+      await preferences.setString('authToken', token);
+
+      return true;
+    }
+    else {
+      print('Failed to register: ${response.body}');
+      return false;
+    }
+  }
+
+  Future<String?> getToken() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return preferences.getString('authToken');
+  }
+
+  Future<String?> getUserRole() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    print(preferences.getString('userRole'));
+    return preferences.getString('userRole');
+  }
+
+  Future<bool> isTokenExpired() async {
+    String? token = await getToken();
+
+    if (token != null) {
+      DateTime expiryDate = Jwt.getExpiryDate(token)!;
+      return DateTime.now().isAfter(expiryDate);
+    }
+
+    return true;
+  }
 }
